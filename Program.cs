86d2e1e6ad34a50ff809905bff9bdade5a31d40e
PC <PC@DESKTOP-NMS16VB>
2025-11-26 22:29:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            decimal x = 1;
+            double y = 3.14;
+            decimal z = 1.1231347234235m; //16 знаков? Используйте суффикс М; Убери его и прочитай комментарий от VS
+            decimal asd = z + x;
+            Console.WriteLine(asd);
+            Console.ReadLine();
+        }
+    }
+}
